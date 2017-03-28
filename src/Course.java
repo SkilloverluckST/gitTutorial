9eead54c5af54b9662754c941f166b05900ceca6
SkilloverluckST <sridley4@uwo.ca>
2017-03-28 16:00:05 -0400

    Added Course

---
+++ src/Course.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Spencer on 28/03/2017.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description)
+    {
+        this.cname = cname;
+        this.description = description;
+    }
+}
